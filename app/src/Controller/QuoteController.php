24831b1b5d566b42ad5389882cbb1ed490180e40
Bruno Casali <brunoocasali@gmail.com>
2022-07-13 15:44:29 -0300

    Create a basic search route to search through quotes

--- app/src/Controller/QuoteController.php
+++ app/src/Controller/QuoteController.php
@@ -5,20 +5,35 @@ namespace App\Controller;
 use App\Repository\QuoteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Quote;
+
+use MeiliSearch\Bundle\SearchService;
 
 class QuoteController extends AbstractController {
 
-    #[Route('/', name: 'index')]
-    public function index(
-        QuoteRepository $quoteRepository
-    )
-    : Response {
-
+    #[Route('/')]
+    public function index(QuoteRepository $quoteRepository) : Response 
+    {
         return $this->render(
             'quote/index.html.twig',
             [
                 'quotes' => $quoteRepository->findAll(),
+            ]
+        );
+    }
+
+    #[Route('/search')]
+    public function search(SearchService $searchService, Request $request): Response 
+    {
+        $searchQuery = $request->query->get('q') ?? '';
+
+        return $this->render(
+            'quote/index.html.twig',
+            [
+                'quotes' => $searchService->search($this->getDoctrine()->getManager(), Quote::class, $searchQuery),
+                'q' => $searchQuery
             ]
         );
     }
